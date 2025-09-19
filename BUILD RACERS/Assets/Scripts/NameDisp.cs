7779a6598c949db96ace29640f9357768cc838ef
A23009 <san-a23009@sist.ac.jp>
2025-09-19 10:46:07 +0900

    unnoHirotoからのマージ

--- BUILD RACERS/Assets/Scripts/NameDisp.cs
+++ BUILD RACERS/Assets/Scripts/NameDisp.cs
@@ -7,15 +7,10 @@ public class NameDisp : MonoBehaviourPunCallbacks
     private void Start()
     {
         var nameLabel = GetComponent<TextMeshPro>();
-<<<<<<< HEAD
 
         var player = photonView.Owner;
 
         int number = System.Array.IndexOf(PhotonNetwork.PlayerList, player);
         nameLabel.text = $"{photonView.Owner.NickName}{number + 1}";
-=======
-        // プレイヤー名とプレイヤーIDを表示する
-        nameLabel.text = $"{photonView.Owner.NickName}({(PhotonNetwork.PlayerList).Length})";
->>>>>>> main
     }
 }
