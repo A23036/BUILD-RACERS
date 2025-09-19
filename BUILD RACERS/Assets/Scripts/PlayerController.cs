e0ab4eb2beb232c538069a7ff33c5e52bea10926
A23009 <san-a23009@sist.ac.jp>
2025-09-19 12:06:25 +0900

    フレームレートによってプレイヤーが動かない問題を改善

--- BUILD RACERS/Assets/Scripts/PlayerController.cs
+++ BUILD RACERS/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviourPunCallbacks
 
     public float jumpPower;
 
+    private Vector3 move;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -20,24 +22,32 @@ public class PlayerController : MonoBehaviourPunCallbacks
         //自分でなければ処理しない
         if (photonView.IsMine == false) return;
 
-        Vector3 move = Vector3.zero;
+    }
+
+    //フレームレートによる差が出ないように、物理演算はこちらで行う
+    private void FixedUpdate()
+    {
+        //自分でなければ処理しない
+        if (photonView.IsMine == false) return;
+
+        move = Vector3.zero;
 
         //キーボード処理
-        if(Keyboard.current != null)
+        if (Keyboard.current != null)
         {
-            if (Keyboard.current.wKey.isPressed) {move.z += moveSpeed;Debug.Log("input W Key");}
-            if (Keyboard.current.sKey.isPressed) {move.z -= moveSpeed;Debug.Log("input S Key");}
-            if (Keyboard.current.aKey.isPressed) {move.x -= moveSpeed;Debug.Log("input A Key");}
-            if (Keyboard.current.dKey.isPressed) {move.x += moveSpeed;Debug.Log("input D Key");}
+            if (Keyboard.current.wKey.isPressed) { move.z += moveSpeed; Debug.Log("input W Key"); }
+            if (Keyboard.current.sKey.isPressed) { move.z -= moveSpeed; Debug.Log("input S Key"); }
+            if (Keyboard.current.aKey.isPressed) { move.x -= moveSpeed; Debug.Log("input A Key"); }
+            if (Keyboard.current.dKey.isPressed) { move.x += moveSpeed; Debug.Log("input D Key"); }
         }
 
         //コントローラー処理
         if (Gamepad.current != null)
         {
-            if (Gamepad.current.buttonNorth.isPressed) {move.z += moveSpeed;Debug.Log("input Y Button");}
-            if (Gamepad.current.buttonSouth.isPressed) {move.z -= moveSpeed;Debug.Log("input A Button");}
-            if (Gamepad.current.buttonWest.isPressed)  {move.x -= moveSpeed;Debug.Log("input X Button");}
-            if (Gamepad.current.buttonEast.isPressed)  {move.x += moveSpeed;Debug.Log("input B Button");}
+            if (Gamepad.current.buttonNorth.isPressed) { move.z += moveSpeed; Debug.Log("input Y Button"); }
+            if (Gamepad.current.buttonSouth.isPressed) { move.z -= moveSpeed; Debug.Log("input A Button"); }
+            if (Gamepad.current.buttonWest.isPressed) { move.x -= moveSpeed; Debug.Log("input X Button"); }
+            if (Gamepad.current.buttonEast.isPressed) { move.x += moveSpeed; Debug.Log("input B Button"); }
         }
 
         if (move != Vector3.zero) rb.AddForce(move);
