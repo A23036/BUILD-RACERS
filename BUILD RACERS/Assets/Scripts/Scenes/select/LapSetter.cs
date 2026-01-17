1198163e98a3f59cde8049a3ecd82b352c6b0ed3
UNNOHIROTO\Amore <san-a23009@sist.ac.jp>
2026-01-17 13:35:52 +0900

    バグの修正＋要素追加(作業中) ・リザルトUIを取得できない問題を修正 ・セレクトシーンからラップ数決められるように作業中

---
+++ BUILD RACERS/Assets/Scripts/Scenes/select/LapSetter.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class LapSetter : MonoBehaviour
+{
+    private int lapCnt = 0;
+    [SerializeField] private int minLapCnt = 0;
+    [SerializeField] private int maxLapCnt = 9;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InputText()
+    {
+        var lapInput = GameObject.Find("lapInputField").GetComponent<TMP_InputField>();
+
+        //êîílïœä∑
+        if (!int.TryParse(lapInput.text, out int lap))
+        {
+            lapInput.text = "";
+            return;
+        }
+
+        lap = Mathf.Clamp(lap, minLapCnt, maxLapCnt);
+        lapCnt = lap;
+        lapInput.text = lap.ToString();
+    }
+}
